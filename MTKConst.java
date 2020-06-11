f0573bc940aaeaebbacdf1e461cac849f5a7d91e
SETH <seth@methinks.io>
2020-06-11 11:55:28 -0700

    basefeature configuration

--- MTKConst.java
+++ MTKConst.java
@@ -26,7 +26,7 @@ public class MTKConst {
     public static final String ROOM_TYPE_APP_TEST = "apptest";
     public static final String ROOM_TYPE_USER_TEST = "usertest";
 
-    public static final String BASE_FEATURE_BUSINESS = "business_app";
+    public static final String BASE_FEATURE_THINKER = "thinker_app";
     public static final String BASE_FEATURE_APPTEST = "apptest_sdk";
 
     public static final int REQUEST_SCREEN_SHARING = 12322;
